e9caf1bc629bf96cf763e37a9193c6e8db14b6a1
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-07-16 22:30:04 -0400

    First Commit

---
+++ src/Challenge.java
@@ -0,0 +1,21 @@
+public class Challenge {
+    public static void main(String[] args) {
+
+
+        double x = 20.0d;
+        double y = 80.0d;
+
+        double result = (x + y) * 100;
+        double remainder = result % 40.00;
+        boolean noRemainder = (remainder == 0) ? true : false;
+
+        System.out.println(noRemainder);
+
+        if(!noRemainder){
+            System.out.println("Got some remainder! A remainder of: " + remainder);
+        } else {
+            System.out.println("There is no remainder!");
+        }
+
+    }
+}
